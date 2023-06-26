947828e6558ba7ed6aa39f134b8cdeb3204e60d1
Rafael <rafaelmoura7033@gmail.com>
2023-06-26 09:38:30 -0300

    Creating main, header and stopwatch component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { StopwatchComponent } from './components/stopwatch/stopwatch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    StopwatchComponent
   ],
   imports: [
     BrowserModule
